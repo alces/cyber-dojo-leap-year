a0ed1f39bae0e979159eaeeb64278487d904668c
QPuA1x <QPuA1x@cyber-dojo.org>
2024-05-16 06:55:50 +0000

    11 ran tests, predicted green, got red

--- files/leap_year.go
+++ files/leap_year.go
@@ -1,6 +1,8 @@
 package leapyear
 
-func isLeap(year int) (leap bool) {
+func isLeap(year int) bool {
+    leap := false
+ 
     if year%4 == 0 {
         if year%100 == 0 {
             if year%400 == 0 {
