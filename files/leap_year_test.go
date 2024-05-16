e9830a8f099ac4da601e7c6305f46e2ba27f77b5
QPuA1x <QPuA1x@cyber-dojo.org>
2024-05-16 06:43:04 +0000

    7 ran tests, predicted red, got red

--- files/leap_year_test.go
+++ files/leap_year_test.go
@@ -16,3 +16,7 @@ func Test1917ShouldNotBeLeap(t *testing.T) {
 func Test1980ShouldBeLeap(t *testing.T) {
     assert.True(t, isLeap(1980))
 }
+
+func Test2000ShouldBeLeap(t *testing.T) {
+    assert.True(t, isLeap(2000))
+}
